adfebac4dfb584b56dacd0131752d163b0316915
Jon Ursenbach <jon@ursenba.ch>
2025-02-14 16:44:44 -0800

    fix: problem where an `undefined loc` error can be sometimes generated

--- src/validation-errors/__tests__/main.test.js
+++ src/validation-errors/__tests__/main.test.js
@@ -1,4 +1,4 @@
-import { openapi } from '@apidevtools/openapi-schemas';
+import { openapi } from '@readme/openapi-schemas';
 import Ajv from 'ajv/dist/2020';
 import { describe, expect, it } from 'vitest';
 
@@ -48,13 +48,13 @@ describe('Main', () => {
     expect(res).toMatchSnapshot();
   });
 
-  // Though this is testing `patternProperties` cases, the error that'll come back will just be for `pattern` since
-  // that's what `patternPropeties` utilizes in the schema.
+  // Though this is testing `patternProperties` cases, the error that'll come back will just be for
+  // `pattern` since that's what `patternPropeties` utilizes in the schema.
   it('should support js output format for patternProperties errors', async () => {
     const [, data] = await getSchemaAndData('patternProperties', __dirname);
 
-    // The OpenAPI 3.1 schema has a good example of this for component names so we're using that instead of writing
-    // our own.
+    // The OpenAPI 3.1 schema has a good example of this for component names so we're using that
+    // instead of writing our own.
     const schema = openapi.v31;
     const ajv = new Ajv({ allErrors: true, strict: false, validateFormats: false });
     const validate = ajv.compile(schema);
